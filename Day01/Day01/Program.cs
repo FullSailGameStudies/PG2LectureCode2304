7f0a1ec1bc8c602716850afb92cd66d0b7ce24a1
Garrett Girod <girodg@hotmail.com>
2023-04-03 12:08:02 -0400

    average challenge

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -115,8 +115,21 @@ namespace Day01
                 When you want to create a List variable, replace T with whatever type of data you want to store in the List.
             */
             string[] heroes = new string[10];
-            List<string> names = new List<string>(); //this list stores strings and only strings.
-
+            List<string> names = new List<string>(10) { "Batman" };//this list stores strings and only strings.
+            PrintInfo(names);//Count: 1 Capacity: 1? 3? 4?
+            names.Add("The Greatest Detective");
+            names.Add("The Caped Crusader");
+            names.Add("The Best");
+            names.Add("Bruce");
+            PrintInfo(names);//Count: 5 Capacity: 8, 6, 4??
+            names.Add("Joker");
+            names.Add("Penguin");
+            names.Add("Bane");
+            names.Add("Riddler");
+            PrintInfo(names);//Count: 9 Capacity: 12? 16? 14? 2.5?????
+            names.Add("Poison Ivy");
+            names.Add("Catwoman");
+            PrintInfo(names);//Count: 11 Capacity: 20? 16??
             /*
                 CHALLENGE 2:
 
@@ -154,7 +167,12 @@ namespace Day01
                     Add a few grades to the grades list you created in CHALLENGE 2.
              
             */
-
+            Random rando = new();
+            grades.Add((float)rando.NextDouble() * 100);
+            grades.Add((float)rando.NextDouble() * 100);
+            grades.Add((float)rando.NextDouble() * 100);
+            grades.Add((float)rando.NextDouble() * 100);
+            grades.Add((float)rando.NextDouble() * 100);
 
 
 
@@ -171,11 +189,52 @@ namespace Day01
                 1) for loop. use the Count property in the for condition.
                 2) foreach loop
             */
+
             for (int i = 0; i < letters.Count; i++)
+            {
                 Console.Write($" {letters[i]}");
+                letters[i] = 'b';
+            }
 
             foreach (var letter in letters)
                 Console.Write($" {letter}");
+
+            /*
+             * Challenge 3.5
+             *   loop over the grades list and print out each grade
+             */
+            for (int i = 0; i < grades.Count; i++)
+            {
+
+            }
+
+            Console.WriteLine("\n\n--PG2 Grades--");
+            foreach (var grade in grades)
+            {
+                //if (grade < 59.5) Console.ForegroundColor = ConsoleColor.Red;
+                //else if(grade < 69.5) Console.ForegroundColor = ConsoleColor.DarkYellow;
+                //else if (grade < 79.5) Console.ForegroundColor = ConsoleColor.Yellow;
+                //else if (grade < 89.5) Console.ForegroundColor = ConsoleColor.Blue;
+                //else Console.ForegroundColor = ConsoleColor.Green;
+
+                //ternary operator
+                // (condition) 
+                // ? - what happens when the condition is true
+                // : - what happens when the condition is false
+                // an if-else
+                Console.ForegroundColor = (grade < 59.5) ? ConsoleColor.Red :
+                                          (grade < 69.5) ? ConsoleColor.DarkYellow :
+                                          (grade < 79.5) ? ConsoleColor.Yellow :
+                                          (grade < 89.5) ? ConsoleColor.Blue :
+                                          ConsoleColor.Green;
+
+                //,7 - will right-align in 7 spaces
+                //:N2 - format as a number with 2 decimal places
+                Console.WriteLine($"{grade,7:N2}");
+
+                Console.ResetColor();
+            }
+
 
             /*
                 CHALLENGE 4:
@@ -185,9 +244,19 @@ namespace Day01
                     3) print the average that is returned.
              
             */
+            float average = t1000.Average(grades);
+            Console.WriteLine($"Average grade: {average}");
 
 
             Console.ReadKey(true);
+        }
+
+        private static void PrintInfo(List<string> names)
+        {
+            //Count: # of items that have been ADDED
+            //Capacity: size (length) of the internal array
+            Console.WriteLine($"Count: {names.Count}\tCapacity: {names.Capacity}");
+
         }
 
         private static void PrintSum(int sum)
@@ -219,8 +288,11 @@ namespace Day01
         public float Average(List<float> numbers)
         {
             float avg = 0F;
-
+            float sum = 0;
             //loop over the numbers and calculate the average
+            for (int i = 0; i < numbers.Count; i++)
+                sum += numbers[i];
+            avg = sum / numbers.Count;
 
             return avg;
         }
