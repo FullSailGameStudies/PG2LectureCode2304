2ac7303402612725e32ef272312a9406965245d4
Garrett Girod <girodg@hotmail.com>
2023-04-10 09:58:09 -0400

    bubble sort

--- Day04/Day04/Program.cs
+++ Day04/Day04/Program.cs
@@ -7,8 +7,81 @@ namespace Day04
 {
     internal class Program
     {
+        /*
+         *   
+            procedure bubbleSort(A : list of sortable items)
+                n := length(A)
+                repeat
+                    swapped := false
+                    for i := 1 to n - 1 inclusive do
+                        if A[i - 1] > A[i] then
+                            swap(A, i - 1, i)
+                            swapped = true
+                        end if
+                    end for
+                    n := n - 1
+                while swapped
+            end procedure
+         */
+        static void BubbleSort(List<int> unsorted)
+        {
+            int n = unsorted.Count;
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 1; i <= n - 1; i++)
+                {
+                    if (unsorted[i - 1] > unsorted[i])
+                    {
+                        //swap(A, i - 1, i);
+                        //int temp = A[i - 1];
+                        //A[i - 1] = A[i];
+                        //A[i] = temp;
+                        (unsorted[i], unsorted[i - 1]) = (unsorted[i - 1], unsorted[i]);
+                        swapped = true;
+                    }
+                }
+                --n;
+            } while (swapped);
+        }
         static void Main(string[] args)
         {
+            List<int> nums = new() { 5, 420, 13, 1, 7 };
+            BubbleSort(nums);
+            foreach (int x in nums)
+                Console.Write($"{x} ");
+            Console.WriteLine(  );
+
+            /*
+                ╔═══════╗ 
+                ║Sorting║
+                ╚═══════╝ 
+             
+                Sorting is used to order the items in a list/array is a specific way
+             
+                CHALLENGE 2:
+
+                    Convert this BubbleSort pseudo-code into a C# method             
+                     
+                    procedure bubbleSort(A : list of sortable items)
+                      n := length(A)
+                      repeat
+                          swapped := false
+                          for i := 1 to n - 1 inclusive do
+                              if A[i - 1] > A[i] then
+                                  swap(A, i - 1, i)
+                                  swapped = true
+                              end if
+                          end for
+                          n := n - 1
+                      while swapped
+                    end procedure
+                    
+            */
+
+
+
             //Recursion, Sorting, Searching
 
             /*
@@ -41,34 +114,6 @@ namespace Day04
 
 
 
-
-
-            /*
-                ╔═══════╗ 
-                ║Sorting║
-                ╚═══════╝ 
-             
-                Sorting is used to order the items in a list/array is a specific way
-             
-                CHALLENGE 2:
-
-                    Convert this BubbleSort pseudo-code into a C# method             
-                     
-                    procedure bubbleSort(A : list of sortable items)
-                      n := length(A)
-                      repeat
-                          swapped := false
-                          for i := 1 to n - 1 inclusive do
-                              if A[i - 1] > A[i] then
-                                  swap(A, i - 1, i)
-                                  swapped = true
-                              end if
-                          end for
-                          n := n - 1
-                      while swapped
-                    end procedure
-                    
-            */
 
         }
 
