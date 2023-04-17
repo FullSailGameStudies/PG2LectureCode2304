4585c38ae93b5c3e18ddb86fce66dc7c36044123
Garrett Girod <girodg@hotmail.com>
2023-04-17 10:06:35 -0400

    challenges 1,2,3

---
+++ Day07/Day07CL/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07CL
+{
+    public class Person
+    {
+        #region Fields
+        private int _age;
+        #endregion
+
+        #region Properties
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value >= 0 && value < 120)
+                    _age = value;
+            }
+        }
+
+        public string Name { get; set; }
+
+        #endregion
+    }
+}
